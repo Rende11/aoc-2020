81676408afc79f24704a1d8fa5957cb9120c5da8
rende11 <artamonovn@gmail.com>
2020-12-08 13:26:23 +0400

    day 2 part 2

--- src/advent_of_code/day_2/core.clj
+++ src/advent_of_code/day_2/core.clj
@@ -29,3 +29,45 @@
   (solution (get-data input-path))
 ;; => 550
 )
+
+;; Part 2
+(defn valid-pass-strict? [line]
+  (let [[range char password] line
+        [pos1* pos2*] (str/split range #"-")
+        pos1 (dec (Integer/parseInt pos1*))
+        pos2 (dec (Integer/parseInt pos2*))
+        splitted  (str/split password #"")
+        char1 (nth splitted pos1)
+        char2 (nth splitted pos2)]
+    (and
+     (not= char1 char2)
+     (or
+      (= char char1)
+      (= char char2)))))
+
+(filter #(= % \l) "hello")
+
+
+(defn solution-2 [data]
+  (count (filter valid-pass-strict? data)))
+
+(comment
+  (solution-2 (get-data input-path))
+;; => 634
+  )
+
+(comment
+  ;; Tips from Fred Overflow solution
+  ;; 1. Use re-matches to parse one line
+  (re-matches #"(\d+)-(\d+) (\w:) (\w+)" "1-3 a: abcde")
+;; => ["1-3 a: abcde" "1" "3" "a:" "abcde"]
+
+;;   2. Use lazy file reader func "with-open"
+;;   3. You can filter chars from string do not need to split it
+  (filterv #(= % \l) "hello")
+;;   4. Exclusive or
+  ;; (not=
+  ;;  (= a b)
+  ;;  (= a c))
+
+  )
