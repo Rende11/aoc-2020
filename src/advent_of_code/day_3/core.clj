d24fa613ca90a81883b0a88bb2cb2a7cc220e703
rende11 <artamonovn@gmail.com>
2020-12-13 18:57:40 +0400

    day 3 part 2

--- src/advent_of_code/day_3/core.clj
+++ src/advent_of_code/day_3/core.clj
@@ -9,18 +9,18 @@
     (doall
      (line-seq rdr))))
 
-(defn build-map [map-part]
+(defn build-map [map-part mult]
   (let [lines-count (count map-part)
         lines-width (count (first map-part))
-        min-width (* lines-count 3)
+        min-width (* lines-count mult)
         fragments-count (int (Math/ceil (/  min-width lines-width)))]
     (apply (partial map str) (repeat fragments-count map-part))))
 
-(defn trace-coords [full-map]
+(defn trace-coords [full-map x-step y-step]
   (let [lines-count (count full-map)]
     (->> [0 0]
          (iterate (fn [[a b]]
-                    [(+ a 3) (+ b 1)]))
+                    [(+ a x-step) (+ b y-step)]))
          (take-while (fn [[_a b]]
                        (<= b lines-count))))))
 
@@ -28,14 +28,30 @@
   (map (fn [[a b]]
          (get-in full-map [b a])) coords))
 
-(defn solution [data]
-  (let [full-map (build-map data)
-        coords (trace-coords full-map)
-        path (acc-path (vec full-map) coords)]
+(defn solution [data x-step y-step]
+  (let [full-map (build-map data x-step)
+        coords   (trace-coords full-map x-step y-step)
+        path     (acc-path (vec full-map) coords)]
     (count (filter #(= \# %) path))))
 
 
 (comment
-  (solution (get-data input-path))
+  (solution (get-data input-path) 3 1)
 ;; => 265
+  )
+
+
+(def steps
+  [[1 1]
+   [3 1]
+   [5 1]
+   [7 1]
+   [1 2]])
+
+(comment
+;; => ;;part 2
+  (->> steps
+       (map #(apply (partial solution (get-data input-path)) %))
+       (reduce *))
+  ;; => 3154761400
   )
